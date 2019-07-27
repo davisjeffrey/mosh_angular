516afac165b55d869a12faa13f7809980022aff7
Jeff Davis <jeff.davis@att.net>
2019-07-27 16:38:20 -0500

    Added authors componenet and service

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,22 @@
+import { AuthorsService } from '../authors.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+  authors;
+  authorCount;
+
+constructor(service: AuthorsService) {
+
+  this.authors = service.getAuthors();
+  this.authorCount = this.authors.length;
+}
+
+ngOnInit() {
+}
+
+}
