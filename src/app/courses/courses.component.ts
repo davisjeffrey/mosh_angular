6ea5b1b86a0a8a092431a37926df6f2075f8a86d
Jeff Davis <jeff.davis@att.net>
2019-08-20 20:22:03 -0500

    Completed Event Handling - 8

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -10,6 +10,12 @@ import { Component } from '@angular/core';
 export class CoursesComponent {
   courses;
   title = 'List of Courses';
+  isActive = true;
+
+  onSave($event) {
+    $event.stopPropagation();
+    console.log('Button was clicked', $event); }
+  onDivClicked() { console.log('Div Button was clicked'); }
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
