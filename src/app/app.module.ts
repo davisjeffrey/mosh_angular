7f9e9fd26099a04465a30dd801c4577494b1e63a
Jeff Davis <jeff.davis@att.net>
2019-09-07 20:55:50 -0500

    Smile to frown with click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     AuthorsComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
 
   ],
   imports: [
---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent {
+
+isHappy: boolean;
+
+onClick() { this.isHappy = !this.isHappy; }
+
+}
